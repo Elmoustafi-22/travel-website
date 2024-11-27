f615d35598e1e22275127aca7b8243b52ee4b77f
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-11-27 08:46:07 +0100

    Added the not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import lostman from "../../public/lost-man.png"
+
+function NotFound() {
+  return (
+    <section className='w-full h-screen flex justify-center items-center flex-col '>
+        <Image 
+            src={lostman}
+            alt='Lost Man 404'
+            width={700}
+            height={450}
+        />
+        <h1 className='text-5xl text-green font-heading font-bold mb-5'>Oops!</h1>
+        <h2 className='text-xl text-black mb-5'>Your are lost... We can&apos;t find the page you&apos;re looking for.</h2>
+        <Link 
+            href="/"
+            className='bg-green text-white hover:bg-darkGray font-bold text-lg py-1 px-4 rounded-full transition-all cursor-pointer'
+        >&larr; Back To Home</Link>
+    </section>
+  )
+}
+
+export default NotFound
