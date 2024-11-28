b4086e771f1400fc382bfa953dadfc911b08e91f
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-11-28 18:06:09 +0100

    Added a Booking Form component

---
+++ src/components/BookingForm.tsx
@@ -0,0 +1,132 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { useForm } from "react-hook-form";
+
+function BookingForm({
+    pkgName, 
+    location,
+    popStatus,
+    closePop
+}: {
+    pkgName:string;
+    location: string;
+    popStatus: boolean;
+    closePop: (status: boolean) => void;
+}) {
+  const {
+    register, 
+    reset, 
+    handleSubmit, 
+    formState: {errors},
+    setValue,
+} = useForm();
+  const [msg, setMsg] = useState<string>();
+
+  useEffect(() => {
+    setValue("pkgName", pkgName);
+    setValue("location", location);
+  }, )
+  const onBooking = (data: any) => {
+    if (data) {
+        console.log(data)
+        setMsg("Thanks for your interest... Back to you shortly!")
+        reset()
+
+        let timer = setTimeout(() => {
+            setMsg("");
+            clearInterval(timer);
+        }, 2000)
+    }
+  }
+  return (
+    popStatus && (   
+        <section className='fixed z-50 top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-80'>
+        <aside className='bg-green p-7 rounded 2xl w-full max-w-[450px] shadow-md relative'>
+            <a 
+                onClick={() => closePop(false)} 
+                className='absolute -right-5 -top-3 cursor-pointer'
+            >
+                <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 128 128"
+                        className="w-10 h-10"
+                        >
+                        <circle cx="64" cy="64" r="64" fill="#c93636" />
+                        <path
+                            d="m100.3,90.4l-26.4-26.4,26.3-26.4c.4-.4.4-1,0-1.4l-8.5-8.5c-.4-.4-1-.4-1.4,0l-26.3,26.4-26.3-26.3c-.4-.4-1-.4-1.4,0l-8.5,8.5c-.4.4-.4,1,0,1.4l26.2,26.3-26.3,26.3c-.4.4-.4,1,0,1.4l8.5,8.5c.4.4,1.1.4,1.4,0l26.4-26.3,26.3,26.3c.4.4,1.1.4,1.5.1l8.5-8.5c.4-.4.4-1,0-1.4Z"
+                            fill="#fff"
+                />
+                </svg>
+            </a>
+            <h2 className='text-center text-2xl font-semibold font-body text-white'>
+                <small>Booking For</small>
+                <br />
+                {pkgName} - {location}
+            </h2>
+            {msg && <p className='text-black text-sm font-body text-center font-semibold'>
+                {msg}    
+            </p>
+            }
+            <form onSubmit={handleSubmit(onBooking)}>
+                <div className="my-3">
+                    <h4 className='text-white font-semibold text-sm mb-1 font-body'>Your Name</h4>
+                    <input 
+                        type="text" 
+                        placeholder='Your name'  
+                        className='w-full text-base px-3 py-1.5 shadow-md outline-none font-body rounded'
+                        {...register("name", {required: true})}
+                    />
+                    {errors.name && (
+                        <p className='font-body text-sm text-white font-medium'>
+                            Please Mention Your Name
+                        </p>
+                    )}
+                </div>
+                <div className="my-3">
+                    <h4 className='text-white font-semibold text-sm mb-1 font-body'>Email</h4>
+                    <input 
+                        type="email" 
+                        placeholder='E-mail Address'  
+                        className='w-full text-base px-3 py-1.5 shadow-md outline-none font-body rounded'
+                        {...register("email", {required: true})}
+                    />
+                    {errors.email && (
+                        <p className='font-body text-sm text-white font-medium'>
+                            Please Mention Your Contact E-mail
+                        </p>
+                    )}
+                </div>
+                <div className="my-3">
+                    <h4 className='text-white font-semibold text-sm mb-1 font-body'>Contact No.</h4>
+                    <input 
+                        type="tel" 
+                        placeholder='Contact Number'  
+                        className='w-full text-base px-3 py-1.5 shadow-md outline-none font-body rounded'
+                        {...register("contactNo", {required: true})}
+                    />
+                    {errors.contactNo && (
+                        <p className='font-body text-sm text-white font-medium'>
+                            Please Mention Your Contact Number
+                        </p>
+                    )}
+                </div>
+                <div className="my-3">
+                    <h4 className='text-white font-semibold text-sm mb-1 font-body'>Details</h4>
+                    <textarea 
+                        placeholder='Details' 
+                        className='w-full text-base px-3 py-1.5 shadow-md outline-none min-h-24 font-body rounded'
+                        {...register("details")}
+                    >
+                    </textarea>
+                </div>
+                <div className='flex justify-end mt-5'>
+                    <button type="submit" className='bg-white text-black hover:bg-darkGray hover:text-white transition-all ease-linear py-2 px-5 rounded font-body font-medium uppercase shadow-md'>Send</button>
+                </div>
+            </form>
+        </aside>
+    </section>
+     )
+  )
+}
+
+export default BookingForm
--- src/components/packageDetails.tsx
+++ src/components/packageDetails.tsx
@@ -1,29 +1,41 @@
+"use client";
 import { Package } from "@/models/package";
-import Link from "next/link";
+import { useState } from "react";
+import BookingForm from "./BookingForm";
+
 
 const PackageDetails = ({pkgInfo}: {pkgInfo: Package}) => {
+    const [popStatus, setPopStatus] = useState(false)
     return (
-        <aside 
-            className="group w-1/3 bg-cover bg-center"
-            style={{ backgroundImage: `url(/${pkgInfo.bgImg})` }}
-        >
-            <div 
-                className="opacity-0 group-hover:opacity-100 bg-black bg-opacity-75 w-full h-[550px] flex flex-col justify-center items-center transition duration-150"
+        <>
+            <aside 
+                className="group w-1/3 bg-cover bg-center"
+                style={{ backgroundImage: `url(/${pkgInfo.bgImg})` }}
             >
-                <h3 className="text-white text-center uppercase text-4xl font-semibold">
-                    {pkgInfo.name}
-                    <br />
-                    <small className="text-xl font-normal tracking-widest">{pkgInfo.location}</small>
-                </h3>
-                <h4 className="text-3xl my-6 text-white font-semibold">{pkgInfo.price}</h4>
-                <Link 
-                    href="#"
-                    className="bg-green text-white font-semibold px-5 py-1.5 rounded-full"
+                <div 
+                    className="opacity-0 group-hover:opacity-100 bg-black bg-opacity-75 w-full h-[550px] flex flex-col justify-center items-center transition duration-150"
                 >
-                    Book Now
-                </Link>
-            </div>
-        </aside>
+                    <h3 className="text-white text-center uppercase text-4xl font-semibold">
+                        {pkgInfo.name}
+                        <br />
+                        <small className="text-xl font-normal tracking-widest">{pkgInfo.location}</small>
+                    </h3>
+                    <h4 className="text-3xl my-6 text-white font-semibold">{pkgInfo.price}</h4>
+                    <a
+                        onClick={() => setPopStatus(true)}
+                        className="cursor-pointer bg-green text-white font-semibold px-5 py-1.5 rounded-full"
+                    >
+                        Book Now
+                    </a>
+                </div>
+            </aside>
+            <BookingForm 
+                pkgName={pkgInfo.name} 
+                popStatus={popStatus} 
+                location={pkgInfo.location} 
+                closePop={(status) => setPopStatus(status)}
+            />
+        </>
     )
 }
 
